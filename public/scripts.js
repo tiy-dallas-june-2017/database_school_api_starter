b16f5e1662ace6db26359ac5571a787076a57fec
Douglas Hirsh <dhirshjr@gmail.com>
2017-08-15 13:44:31 -0500

    Added student details.

--- public/scripts.js
+++ public/scripts.js
@@ -7,11 +7,33 @@ fetch('/api/students')
   .then(function(results) {
     let studentList = document.querySelector('#studentList');
 
+    studentList.addEventListener('click', function(evt) {
+      console.log(evt.target.getAttribute('data-student-id'));
+      let studentId = evt.target.getAttribute('data-student-id');
+      fetch(`/api/students/${studentId}`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(results) {
+          console.log(results);
+          let studentDetailsElement = document.querySelector('#studentDetails');
+
+          studentDetailsElement.innerHTML = `
+            <div>ID: ${results.student_id}</div>
+            <div>Name: ${results.first_name} ${results.last_name}</div>
+            <div>Email: ${results.email}</div>
+            <div>Gender: ${results.gender}</div>
+          `;
+        });
+    });
+
     //1. Loop the results array using a foreach.
     results.forEach(function(student) {
       let listItem = document.createElement('li');
-      listItem.textContent = `${student.first_name} ${student.last_name}`;
+      listItem.innerHTML = `${student.first_name} ${student.last_name}`;
+      listItem.setAttribute('data-student-id', student.student_id);
       studentList.appendChild(listItem);
       //let listItemMarkup = `<li>${student.firstName}</li>`;
+      //console.log(student);
     });
   });
